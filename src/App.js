cc5f1d6213c589816206d59b7bf6f850fd1d1738
Mbuguacodes254 <lizziecodes254@gmail.com>
2023-11-15 01:05:01 +0300

    assignment 4 completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './Card';
+import logo from './logo.svg';
+import sedanIcon from './icons/sedan.svg';
+import suvIcon from './icons/suv.svg';
+import luxuryIcon from './icons/luxury.svg';
 function App() {
   return (
     <div className="App">
@@ -18,6 +22,26 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="card-container">
+        <Card 
+          bgColor="#f0a30a"
+          icon={sedanIcon}
+          title="SEDANS"
+          description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+        />
+        <Card 
+          bgColor="#0050ef"
+          icon={suvIcon}
+          title="SUVS"
+          description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+        />
+        <Card 
+          bgColor="#a20025"
+          icon={luxuryIcon}
+          title="LUXURY"
+          description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Component/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css'; 
+
+const Card = ({ bgColor, icon, title, description }) => {
+return (
+    <div className="card" style={{ backgroundColor: bgColor }}>
+    <img src={icon} alt={title} className="card-icon" />
+    <h2>{title}</h2>
+    <p>{description}</p>
+    <button className="learn-more">Learn More</button>
+    </div>
+);
+};
+
+export default Card;
